999c66fa65775edf958843b6f07cb195c66e88c2
Ryan Bachman <jacksprat455@gmail.com>
2018-07-11 15:35:56 -0500

    checkin off exercises

---
+++ Example1.py
@@ -0,0 +1,29 @@
+# import os
+#
+# os.system("clear")
+#
+#
+# bandMate ="ringo"
+# print(bandMate[1], bandMate[2], bandMate[3])
+# print(bandMate[2])
+# print(bandMate[3], bandMate[4])
+# print("The name is this long ", len(bandMate))
+# print("Ringo".upper())
+# print("Ringo".lower())
+# name = input("Name: ")
+# print("The entered string is ", len(name), "characters long")
+# name = input("Name: ")
+# otherName = input("Name: ")
+# oneMoreName = input("Name: ")
+# print(len(name))
+# print(len(otherName))
+# print(len(oneMoreName))
+#
+#
+# def longestWord(some_list):
+#     count = 0
+#     for i in some_list:
+#         if len(i) > count:
+#             count = len(i)
+#             word = i
+#     return ("the longest string is " + word)
